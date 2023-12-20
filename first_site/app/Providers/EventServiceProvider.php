d4be39bc8b7a41e09fc3d3058f29d7bb59fb6022
ziregul18 <149310191+ziregul18@users.noreply.github.com>
2023-12-20 20:04:17 +0600

    change

--- first_site/app/Providers/EventServiceProvider.php
+++ first_site/app/Providers/EventServiceProvider.php
@@ -9,20 +9,11 @@ use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
-    /**
-     * The event to listener mappings for the application.
-     *
-     * @var array<class-string, array<int, class-string>>
-     */
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
     ];
-
-    /**
-     * Register any events for your application.
-     */
     public function boot(): void
     {
         //
