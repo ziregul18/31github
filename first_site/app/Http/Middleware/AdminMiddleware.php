1f3d45a45964fd063527df524cb9474bfb70bbcb
ziregul18 <149310191+ziregul18@users.noreply.github.com>
2023-12-25 19:27:35 +0600

    change

--- first_site/app/Http/Middleware/AdminMiddleware.php
+++ first_site/app/Http/Middleware/AdminMiddleware.php
@@ -9,11 +9,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AdminMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle(Request $request, Closure $next): Response
     {
         if (!auth()->user()){
@@ -22,7 +17,6 @@ class AdminMiddleware
         if (auth()->user()->role !== User::ADMIN){
             abort(404);
         }
-
         return $next($request);
     }
 }
