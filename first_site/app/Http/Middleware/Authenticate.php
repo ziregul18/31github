d424c4d7103390145f892a65de2a80c79ddb7e35
ziregul18 <149310191+ziregul18@users.noreply.github.com>
2023-12-26 19:06:29 +0600

    change

--- first_site/app/Http/Middleware/Authenticate.php
+++ first_site/app/Http/Middleware/Authenticate.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
 {
-
     protected function redirectTo(Request $request): ?string
     {
         return $request->expectsJson() ? null : route('login');
